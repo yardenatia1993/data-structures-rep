81f93121b17c2dd7753f920b8178c155a00e4cd7
yardenatia1993 <43033715+yardenatia1993@users.noreply.github.com>
2023-04-18 03:58:01 +0300

    first queue

---
+++ 04_Queue/Queue/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Queue;
+
+namespace Main.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                q.Insert(i+1);
+            }
+            Console.WriteLine(q);
+
+            Console.WriteLine("The first node is removed: " + q.Remove());
+            Console.WriteLine(q);
+
+            Console.WriteLine("The first node is : " + q.Head());
+            Console.WriteLine(q);
+
+            Console.WriteLine("Number Of Nodes=" + q.NumberOfNodes());
+            Console.WriteLine(q);
+
+            Console.WriteLine("Number Of Nodes=" + NumberOfNodes(q));
+            Console.WriteLine(q);
+
+            Console.WriteLine("Number Of Nodes=" + NumberOfNodes(q,-1));
+            Console.WriteLine(q);
+            Console.ReadKey();
+        }
+
+        public static int NumberOfNodes<T>(Queue<T> q)
+        {
+            int counter = 0;
+            Queue<T> temp = new Queue<T>();
+
+            while (!q.IsEmpty())
+            {
+                counter++;
+                temp.Insert(q.Remove());
+            }
+
+            //restore q
+            while (!temp.IsEmpty())
+            {
+                q.Insert(temp.Remove());
+            }
+
+            return counter;
+        }
+
+        public static int NumberOfNodes<T>(Queue<T> q, T dummy)
+        {
+            int counter = 0;
+            q.Insert(dummy);
+
+            while (!q.Head().Equals(dummy))
+            {
+                counter++;
+                q.Insert(q.Remove());
+            }
+
+            //Remove dummy
+            q.Remove();
+
+            return counter;
+        }
+        public static Queue<T> DuplicateQueue<T>(Queue<T> q)
+        {
+            Queue<T> temp = q;
+            return temp;
+        }
+    }
+}
---
+++ 04_Queue/Queue/Queue.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedList;
+
+namespace Queue
+{
+    internal class Queue<T>
+    {
+        private Node<T> first;
+        private Node<T> last;
+
+        public Queue()
+        {
+            this.first = this.last = null;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.first == null;
+        }
+
+        public void Insert(T x)
+        {
+            Node<T> node = new Node<T>(x);
+
+            if (IsEmpty())
+            {
+                first = node;
+            }
+            else
+            {
+                last.SetNext(node);
+            }
+            last = node;
+        }
+
+        public T Remove()
+        {
+            T x = first.GetValue();
+            first = first.GetNext();
+
+            if (first == null)
+            {
+                last = null;
+            }
+
+            return x;
+        }
+
+        public T Head()
+        {
+            return first.GetValue();
+        }
+
+        public override string ToString()
+        {
+            if (IsEmpty())
+            {
+                return "[]";
+            }
+
+            string str = "[";
+            Node<T> temp = first;
+            while (temp != null)
+            {
+                str += temp.GetValue() + ",";
+                temp = temp.GetNext();
+            }
+            //str = str.Substring(0, str.Length - 1) + "]";
+            str += "\b]";
+
+            return str;
+        }
+
+        public int NumberOfNodes()
+        {
+            Node<T> temp = first;
+            int counter = 0;
+
+            while (temp != null)
+            {
+                counter++;
+                temp = temp.GetNext();
+            }
+
+            return counter;
+        }
+    }
+}
